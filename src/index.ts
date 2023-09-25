592b0fdaed5caca98b879ed1b991b7b74484020b
Valentyn Nastenko <valentin.nastenko@doublegood.com>
2023-09-25 19:18:42 +0200

    fix validation process

--- src/index.ts
+++ src/index.ts
@@ -19,55 +19,6 @@ async function getCommitEmail(): Promise<string> {
   return output.trim()
 }
 
-async function getKeyByEmail(email: string): Promise<string> {
-  const url = KEYS_SERVER_URL + 'vks/v1/by-email/' + encodeURIComponent(email)
-  const response = await fetch(url)
-  if (response.status !== 200) {
-    throw new Error(`Error fetching key for email ${email}`)
-  }
-  return await response.text()
-}
-
-async function getPgpKeyId(): Promise<string> {
-  const commit = await execShellCommandPassError("git log -1 --format='%H'")
-  const getCommitCommand = 'git verify-commit '+commit
-  if(DEBUG) {
-    console.log(getCommitCommand)
-  }
-  const output = await execShellCommandPassError(getCommitCommand)
-  if(DEBUG) {
-    console.log("git verify-commit")
-    console.log(output)
-  }
-  const pattern = /using (\w+) key (\w+)/
-  const match = pattern.exec(output)
-  if (!match) {
-    core.setFailed('Commit is not signed')
-    await core.summary.addRaw(`❌ Commit is not signed`).write();
-  } else if (match[1] !== 'RSA') {
-    core.setFailed('You should use an RSA key')
-    await core.summary.addRaw(`❌ You should use an RSA key`).write();
-  }
-  if (match?.[2] !== null) return match![2]
-  return ''
-}
-
-async function getKeyById(keyId: string): Promise<string> {
-  if (keyId === null) core.setFailed('RSA key error')
-  const url = KEYS_SERVER_URL + 'vks/v1/by-keyid/' + encodeURIComponent(keyId)
-  const response = await fetch(url)
-  if (response.status !== 200) {
-    const url2 =
-      KEYS_SERVER_URL + 'vks/v1/by-fingerprint/' + encodeURIComponent(keyId)
-    const response2 = await fetch(url2)
-    if (response2.status !== 200) {
-      core.setFailed(`RSA key not found in ${KEYS_SERVER_URL}`)
-    }
-    return await response2.text()
-  }
-  return await response.text()
-}
-
 async function execShellCommand(command: string): Promise<string> {
   const exec = require('child_process').exec
   return new Promise<string>((resolve, reject) => {
@@ -121,28 +72,20 @@ async function validateCommit() {
         }
     }
 
-    const key = await getKeyByEmail(email)
-    const keyId = await getPgpKeyId()
-    const keyValidation = await getKeyById(keyId)
-    if (DEBUG) {
-      console.log(key)
-      console.log(keyId)
-      console.log(keyValidation)
+    const gitSignatureCommand = 'git log -n 1 --show-signature --format="%h %G?"';
+    const res = await execShellCommandPassError(gitSignatureCommand)
+    const output = res.trim();
+    if (output.endsWith('G')) {
+      core.setOutput('commit', 'Your commit is valid')
+      await core.summary.addRaw("✅ Your commit is valid ").write();
+    } else {
+      core.setFailed('Commit is not signed or not valid')
+      await core.summary.addRaw(`❌ Commit is not signed or not valid`).write();
     }
-
-    const hash1 = crypto.createHash('sha1').update(key).digest('hex')
-    const hash2 = crypto.createHash('sha1').update(keyValidation).digest('hex')
-    if (hash1 !== hash2) {
-      core.setFailed(`Commit is not validated by ${KEYS_SERVER_URL}`)
-      await core.summary.addRaw(`❌ Commit is not validated by ${KEYS_SERVER_URL}`).write();
-    }
-    core.setOutput('commit', 'Your commit is valid')
-    await core.summary.addRaw("✅ Your commit is valid ").write();
   } catch (error) {
     core.setFailed("error: " + error)
   }
 
 }
-
 
 validateCommit()
